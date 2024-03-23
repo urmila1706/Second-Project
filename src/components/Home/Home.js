82346a996107a9e3f1131770f69653f3263c3feb
urmila1706 <raut.urmila88@gmail.com>
2024-03-23 19:48:48 +0530

    Implimenting code by using useContext

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
 const Home = (props) => {
   return (
     <Card className={classes.home}>
+    <Button onClick={props.onLogout}>Logout</Button>
       <h1>Welcome back!</h1>
     </Card>
   );
